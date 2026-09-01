ed98c7cddefb700b9f6073db8dc47b027845a88f
Yahoo <119646314+y4ho0@users.noreply.github.com>
2026-09-02 00:03:37 +0800

    fix(xueqiu): fetch anonymous token from market page

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -744,7 +744,7 @@ class TestXueqiuChannel:
         cookie_names = {c.name for c in xq_mod._cookie_jar}
         assert "xq_a_token" in cookie_names
 
-    def test_ensure_cookies_uses_public_homepage_fallback(self, monkeypatch):
+    def test_ensure_cookies_uses_public_market_page_fallback(self, monkeypatch):
         import agent_reach.channels.xueqiu as xueqiu_mod
 
         monkeypatch.setattr(xueqiu_mod, "_cookies_initialized", False)
@@ -765,7 +765,7 @@ class TestXueqiuChannel:
         )
         xueqiu_mod._ensure_cookies()
 
-        assert requested == ["https://xueqiu.com"]
+        assert requested == ["https://xueqiu.com/hq"]
         assert xueqiu_mod._cookies_initialized is True
 
     def test_get_json_sends_referer_and_browser_ua(self, monkeypatch):
